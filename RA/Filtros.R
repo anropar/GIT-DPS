d9a2f0c7abd43d63d3bf5e04bf25ef52ff6038ca
anropar <andres.romero@unillanos.edu.co>
2021-12-16 09:19:35 -0500

    Actualización filtros

--- RA/Filtros.R
+++ RA/Filtros.R
@@ -30,7 +30,7 @@ Entrega = "E2"
 Consulta_5_P1 = read_delim(paste(Carpeta,"2. Sabana","Salidas","E1", "Consulta_5_E1_02122021.txt", sep = slash), ";", escape_double = FALSE, trim_ws = TRUE)
 Consulta_6_P2 = read_delim(paste(Carpeta,"2. Sabana","Salidas",Entrega, "Consulta_6_E2_xxxxx.txt", sep = slash), ";", escape_double = FALSE, trim_ws = TRUE)
 
-Consulta_AC = rbind(Consulta_5_P1, Consulta_6_P2)
+Consulta_AC = Consulta_5_P1
 # Consulta_5$ACTIVIDAD_PRODUCTIVA = ifelse(grepl("ACTIVIDAD PRODUCTIVA", toupper(Consulta_5$`LOGRO y/o PRIVACIÓN GESTIONADA`)),1,0)
 # Consulta_5_P1$ACTIVIDAD_PRODUCTIVA = ifelse(grepl("ACTIVIDAD PRODUCTIVA", toupper(Consulta_5_P1$`LOGRO y/o PRIVACIÓN GESTIONADA`)),1,0)
 # 
